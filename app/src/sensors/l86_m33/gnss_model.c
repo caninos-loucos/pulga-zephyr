efc22817e36d4e6edc3d03762aac8181d723fc2b
matheusorsi <matheusorsi97@gmail.com>
2024-10-08 13:57:53 -0300

    Fixed GNSS item size, that was causing a bug in saving UTC data. Added content to README.md

--- app/src/sensors/l86_m33/gnss_model.c
+++ app/src/sensors/l86_m33/gnss_model.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <zephyr/logging/log.h>
 #include <sensors/l86_m33/l86_m33_service.h>
 
@@ -22,13 +23,13 @@ static void encode_verbose(uint32_t *data_words, uint8_t *encoded_data, size_t e
 
     // Formats the string
     snprintf(encoded_data, encoded_size,
-             "Latitude: %lld.%02lld o; Longitude: %lld.%02lld o; Bearing angle: %d.%02d o; "
-             "Speed: %d.%02d m/s; Altitude: %d.%02d m;\n\t"
-             "Timestamp: %02dh %02dmin %02d.%02ds - %02d/%02d/20%02d\n",
+             "Latitude: %lld.%lld o; Longitude: %lld.%lld o; Bearing angle: %d.%d o; "
+             "Speed: %d.%d m/s; Altitude: %d.%d m;\n\t"
+             "Timestamp: %02dh %02dmin %02d.%ds - %02d/%02d/20%02d\n",
              gnss_model->navigation.latitude / 1000000000,
-             gnss_model->navigation.latitude % 1000000000,
+             llabs(gnss_model->navigation.latitude % 1000000000) / 100, // Gets 7 digits
              gnss_model->navigation.longitude / 1000000000,
-             gnss_model->navigation.longitude % 1000000000,
+             llabs(gnss_model->navigation.longitude % 1000000000) / 100, // Gets 7 digits
              gnss_model->navigation.bearing / 1000,
              gnss_model->navigation.bearing % 1000,
              gnss_model->navigation.speed / 1000,
--- app/src/sensors/l86_m33/l86_m33_service.h
+++ app/src/sensors/l86_m33/l86_m33_service.h
@@ -5,15 +5,17 @@
 #include <sensors/sensors_interface.h>
 
 // Number of 32-bit words in each data item (model)
-#define GNSS_MODEL_WORDS 9
+// Padding added in navigation field to align
+// by largest member (64-bit fields)
+#define GNSS_MODEL_WORDS 10
 
 typedef struct
 {
-    /** Navigation data acquired */
-	struct navigation_data navigation; // 7 words
+	/** Navigation data acquired */
+	struct navigation_data navigation; // 8 words
 	/** UTC time when data was acquired */
 	struct gnss_time real_time; // 2 words
-    
+
 } SensorModelGNSS;
 
 // Registers Si1133 model callbacks
