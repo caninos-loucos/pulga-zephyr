6cb1a4b096e043ff153971066515c6ba54e5bd4e
matheusorsi <matheusorsi97@gmail.com>
2024-10-30 17:53:22 -0300

    Fixing gnss message by increasing datarate when GNSS and LORA are active

--- app/src/sensors/l86_m33/gnss_model.c
+++ app/src/sensors/l86_m33/gnss_model.c
@@ -25,7 +25,7 @@ static int encode_verbose(uint32_t *data_words, uint8_t *encoded_data, size_t en
     return snprintf(encoded_data, encoded_size,
                     "Latitude: %lld.%lld o; Longitude: %lld.%lld o; Bearing angle: %d.%d o; "
                     "Speed: %d.%d m/s; Altitude: %d.%d m;\n\t"
-                    "Timestamp: %02dh %02dmin %02d.%ds - %02d/%02d/20%02d\n",
+                    "Timestamp: %02dh %02dmin %02d.%ds - %02d/%02d/20%02d",
                     gnss_model->navigation.latitude / 1000000000,
                     llabs(gnss_model->navigation.latitude % 1000000000) / 100, // Gets 7 digits
                     gnss_model->navigation.longitude / 1000000000,
@@ -53,15 +53,15 @@ static int encode_minimalist(uint32_t *data_words, uint8_t *encoded_data, size_t
 
     // Formats the string
     return snprintf(encoded_data, encoded_size,
-                    "LT%llxLG%llxB%xS%xA%xTS%02d%02d%dD%02d%02d%02d\n",
-                    gnss_model->navigation.latitude,
-                    gnss_model->navigation.longitude,
+                    "LT%lldLG%lldB%dS%dA%dTS%02d%02d%dD%02d%02d%02d",
+                    gnss_model->navigation.latitude / 100,
+                    gnss_model->navigation.longitude / 100,
                     gnss_model->navigation.bearing,
                     gnss_model->navigation.speed,
-                    gnss_model->navigation.altitude,
+                    gnss_model->navigation.altitude / 100,
                     gnss_model->real_time.hour,
                     gnss_model->real_time.minute,
-                    gnss_model->real_time.millisecond,
+                    gnss_model->real_time.millisecond / 1000,
                     gnss_model->real_time.month_day,
                     gnss_model->real_time.month,
                     gnss_model->real_time.century_year);
