23c0a2339a13c6bd404c73b973859ab12702ee1f
Kaue Rodrigues <kaue.rodrigueskrb@gmail.com>
2025-02-12 19:18:09 -0300

    Comentários novos adicionados. Trechos inúteis removidos. Funções realocadas para evitar declarações desnecessárias. Antigos comentários refeitos.

--- app/src/sensors/scd30/scd30_service.c
+++ app/src/sensors/scd30/scd30_service.c
@@ -14,19 +14,126 @@ LOG_MODULE_REGISTER(scd30_service, CONFIG_APP_LOG_LEVEL);
 static const struct device *scd30;
 static SensorAPI scd30_api = {0};
 
-// Semaphore that allows read data to be stored in buffer
+// Semaphore to synchronize access to the buffer for storing sensor data
 static struct k_sem store_data;
-
-// Sets the sample time of the aplication into the SCD30 device,
-// clipping it since it only allows measurement time from 2s to 1800s.
-// raw_sample_time is given in ms
-int set_valid_sample_time(int raw_sample_time);
-
-void store_data_callback(void);
 
 /**
  * IMPLEMENTATIONS
  */
+
+/**
+ * Sets a valid sample time for the SCD30 sensor.
+ * Clips the sample time to be within the allowed range of 2s to 1800s, as
+ * specified on the datasheet.
+ * 
+ * @param raw_sample_time The desired sample time in milliseconds.
+ * @return 0 on success, or a negative error code on failure.
+ */
+int set_valid_sample_time(int raw_sample_time)
+{
+    int error = 0;
+    struct sensor_value period;
+
+    raw_sample_time /= 1000;
+
+    // Clip the value using mathemagical properties
+    period.val1 = (int)fmax(2, fmin(raw_sample_time, 180));
+    if (period.val1 != raw_sample_time)
+    {
+        LOG_INF("Samplig period outside SCD30 specification, SCD30 set to sample every %d seconds.",
+                period.val1);
+    }
+
+    // Send the chosen period to the driver
+    error = sensor_attr_set(scd30, SENSOR_CHAN_ALL, SCD30_SENSOR_ATTR_SAMPLING_PERIOD, &period);
+    if (error)
+    {
+        LOG_ERR("Could not set application sample time. Error code: %d", error);
+        return error;
+    }
+
+    return 0;
+}
+
+/**
+ * @brief Callback function to store sensor data.
+ *
+ * This function is triggered to store data from the SCD30 sensor into a buffer.
+ * It first checks if it is allowed to save data by attempting to take a semaphore.
+ * If the semaphore is not available, the function returns immediately.
+ * 
+ * The function retrieves CO2, temperature, and humidity data from the SCD30 sensor,
+ * stores it in a SensorModelSCD30 structure, and then copies this data into a buffer.
+ * 
+ * If the data insertion into the buffer fails, an error message is logged.
+ */
+void store_data_callback(void) {
+    
+    // Returns if it's not supposed to save data to buffer
+    if (k_sem_take(&store_data, K_NO_WAIT)) {
+        return;
+    }
+    
+    LOG_DBG("Storing data");
+
+    SensorModelSCD30 scd30_model;
+    uint32_t scd30_data[MAX_32_WORDS];
+    int error = 0;
+
+
+    sensor_channel_get(scd30, SENSOR_CHAN_CO2,
+                       &scd30_model.co2);
+    sensor_channel_get(scd30, SENSOR_CHAN_AMBIENT_TEMP,
+                       &scd30_model.temperature);
+    sensor_channel_get(scd30, SENSOR_CHAN_HUMIDITY,
+                       &scd30_model.humidity);
+    memcpy(&scd30_data, &scd30_model, sizeof(SensorModelSCD30));
+
+    if (insert_in_buffer(scd30_data, SCD30_MODEL, error) != 0)
+    {
+        LOG_ERR("Failed to insert data in ring buffer.");
+    }
+}
+
+/**
+ * @brief Reads sensor values from the SCD30 sensor and stores them in a buffer.
+ */
+static void read_sensor_values()
+{
+    LOG_DBG("Allowing SCD30 to store fix data in buffer");
+    k_sem_give(&store_data);
+
+    
+    /**
+     *  Note: Uncomment code bellow if you want the application to perform consecultive readings. 
+     *  Otherwise, it will wait the interrupt routine in driver.
+     */
+
+    // LOG_DBG("Reading SCD30");
+
+    // SensorModelSCD30 scd30_model;
+    // uint32_t scd30_data[MAX_32_WORDS];
+    // int error = 0;
+
+    // error = sensor_sample_fetch(scd30);
+    // if (!error)
+    // {
+    //     sensor_channel_get(scd30, SENSOR_CHAN_CO2,
+    //                        &scd30_model.co2);
+    //     sensor_channel_get(scd30, SENSOR_CHAN_AMBIENT_TEMP,
+    //                        &scd30_model.temperature);
+    //     sensor_channel_get(scd30, SENSOR_CHAN_HUMIDITY,
+    //                        &scd30_model.humidity);
+    //     memcpy(&scd30_data, &scd30_model, sizeof(SensorModelSCD30));
+
+    //     if (insert_in_buffer(scd30_data, SCD30_MODEL, error) != 0)
+    //     {
+    //         LOG_ERR("Failed to insert data in ring buffer.");
+    //     }
+    // }
+    // else
+    //     LOG_ERR("fetch_sample failed: %d", error);
+}
 
 // Gets and initializes device
 static int init_sensor()
@@ -62,78 +169,6 @@ static int init_sensor()
     return 0;
 }
 
-// Reads sensor measurements and stores them in buffer
-static void read_sensor_values()
-{
-    LOG_DBG("Allowing SCD30 to store fix data in buffer");
-    k_sem_give(&store_data);
-
-    LOG_DBG("Teste sample fetch antiga");
-
-
-    LOG_DBG("Reading SCD30");
-
-    SensorModelSCD30 scd30_model;
-    uint32_t scd30_data[MAX_32_WORDS];
-    int error = 0;
-
-    error = sensor_sample_fetch(scd30);
-    if (!error)
-    {
-        sensor_channel_get(scd30, SENSOR_CHAN_CO2,
-                           &scd30_model.co2);
-        sensor_channel_get(scd30, SENSOR_CHAN_AMBIENT_TEMP,
-                           &scd30_model.temperature);
-        sensor_channel_get(scd30, SENSOR_CHAN_HUMIDITY,
-                           &scd30_model.humidity);
-        memcpy(&scd30_data, &scd30_model, sizeof(SensorModelSCD30));
-
-        if (insert_in_buffer(scd30_data, SCD30_MODEL, error) != 0)
-        {
-            LOG_ERR("Failed to insert data in ring buffer.");
-        }
-    }
-    else
-        LOG_ERR("fetch_sample failed: %d", error);
-}
-
-void store_data_callback(void) {
-    
-    // Returns if it's not supposed to save data to buffer
-    if (k_sem_take(&store_data, K_NO_WAIT)) {
-        return;
-    }
-    
-    LOG_DBG("Storing data");
-
-    SensorModelSCD30 scd30_model;
-    uint32_t scd30_data[MAX_32_WORDS];
-    int error = 0;
-
-    // error = sensor_sample_fetch(scd30);
-    // if (!error)
-    // {
-
-
-    sensor_channel_get(scd30, SENSOR_CHAN_CO2,
-                       &scd30_model.co2);
-    sensor_channel_get(scd30, SENSOR_CHAN_AMBIENT_TEMP,
-                       &scd30_model.temperature);
-    sensor_channel_get(scd30, SENSOR_CHAN_HUMIDITY,
-                       &scd30_model.humidity);
-    memcpy(&scd30_data, &scd30_model, sizeof(SensorModelSCD30));
-
-    if (insert_in_buffer(scd30_data, SCD30_MODEL, error) != 0)
-    {
-        LOG_ERR("Failed to insert data in ring buffer.");
-    }
-
-        
-    // }
-    // else
-        // LOG_ERR("fetch_sample failed: %d", error);
-}
-
 // Register SCD30 sensor callbacks
 SensorAPI *register_scd30_callbacks()
 {
@@ -142,30 +177,4 @@ SensorAPI *register_scd30_callbacks()
     scd30_api.read_sensor_values = read_sensor_values;
     scd30_api.data_model_api = register_scd30_model_callbacks();
     return &scd30_api;
-}
-
-int set_valid_sample_time(int raw_sample_time)
-{
-    int error = 0;
-    struct sensor_value period;
-
-    raw_sample_time /= 1000;
-
-    // Clip the value using mathemagical properties
-    period.val1 = (int)fmax(2, fmin(raw_sample_time, 180));
-    if (period.val1 != raw_sample_time)
-    {
-        LOG_INF("Samplig period outside SCD30 specification, SCD30 set to sample every %d seconds.",
-                period.val1);
-    }
-
-    // Send the chosen period to the driver
-    error = sensor_attr_set(scd30, SENSOR_CHAN_ALL, SCD30_SENSOR_ATTR_SAMPLING_PERIOD, &period);
-    if (error)
-    {
-        LOG_ERR("Could not set application sample time. Error code: %d", error);
-        return error;
-    }
-
-    return 0;
 }
