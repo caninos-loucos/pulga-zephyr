065944ebd7980432019f356ded7e31218158c2d8
Gabriel Minossi <gabriel.minossi@gmail.com>
2025-10-14 17:33:12 -0300

    refactor: use longer delay between test phases

--- samples/test_sleep_consumption/src/main.c
+++ samples/test_sleep_consumption/src/main.c
@@ -7,34 +7,32 @@
 #include <zephyr/pm/device.h>
 #include <zephyr/pm/device_runtime.h>
 
+#define WAIT_PERIOD K_SECONDS(20)
 int main(void)
 {
 	printf("Starting application");
-	//k_sleep(K_MSEC(1000));
+	k_sleep(WAIT_PERIOD);
 
-	uint32_t aux, aux_time;
-
-    while(1){
+	uint32_t aux = 5;
+	while (1)
+	{
 		printf("Starting sum");
 
-		aux = 0;
-		aux_time = k_uptime_get();
-
-		while(k_uptime_get() - aux_time < 10000){
-
-
-			for (uint32_t i = 0; i < 100000; i++){
-				aux++;
-			}
+		k_timepoint_t timeout = sys_timepoint_calc(WAIT_PERIOD);
+		while (!sys_timepoint_expired(timeout))
+		{
+			aux += aux / 999;
+			aux *= 19867 % 64546;
 		}
 
 		printf("Starting sleep");
-
-		k_sleep(K_MSEC(10000));
+		k_sleep(WAIT_PERIOD);
 
 		printf("Starting busy wait");
+		k_busy_wait(15/10 * WAIT_PERIOD.ticks / CONFIG_SYS_CLOCK_TICKS_PER_SEC * USEC_PER_SEC);
 
-		k_busy_wait(10000000);
+		printf("Starting sleep");
+		k_sleep(WAIT_PERIOD);
 	}
 
 	k_sleep(K_FOREVER);
